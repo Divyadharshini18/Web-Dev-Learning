2ccb3ac0615c93f3e0caafad2d453c64243f85f6
Divyadharshini18 <divyasadhu725@gmail.com>
2026-01-07 11:51:06 +0530

    react static page done

---
+++ React/Task3/react-facts/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return (
+        <header>
+            <nav>
+                <img src="../react-logo.png" alt="" width="40" />
+                <h1>Reactfacts</h1>
+            </nav>
+        </header>
+    )
+}
