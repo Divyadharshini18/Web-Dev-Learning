31cf21b56b05e72fe586fb325133ad346f028327
Divyadharshini18 <divyasadhu725@gmail.com>
2026-01-09 13:12:03 +0530

    App.jsx completed

---
+++ React/task5/src/App.jsx
@@ -0,0 +1,36 @@
+import Header from "./Header";
+import Entry from "./Entry";
+
+function App(){
+  return(
+    <>
+      <Header />
+      <Entry 
+      img="../src/assets/japa.png"
+      country="JAPAN"
+      map="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+      place="Mount Fuji"
+      date="12 Jan, 2021 - 24 Jan, 2021"
+      desc="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+      />
+      <Entry 
+      img="../src/assets/australia.png"
+      country="AUSTRALIA"
+      map="https://maps.app.goo.gl/dhMvgZVcwnhYTF8S9"
+      place="Sydney Opera House"
+      date="27 May, 2023 - 8 Jun, 2023"
+      desc="The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+      />
+      <Entry 
+      img="../src/assets/norway.png"
+      country="NORWAY"
+      map="https://maps.app.goo.gl/ExuMAGogMAjk4jtF7"
+      place="Geirangerfjord"
+      date="01 Oct, 2024 - 18 Nov, 2024"
+      desc="The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+      />
+    </>
+  )
+}
+
+export default App;
